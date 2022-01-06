c15da4ffeee47491e32dabb4bd26f2debce63374
chuhx <chuhx1024@126.com>
2022-01-06 16:51:31 +0800

    feat: 注册用户保存到数据库 password helper.md5

--- app/controller/user.js
+++ app/controller/user.js
@@ -5,16 +5,28 @@ const Controller = require('egg').Controller
 class UserController extends Controller {
     async create() {
         const { ctx } = this
+        // 获取请求体
+        const body = ctx.request.body
         // 1. 数据校验
         ctx.validate({
             username: { type: 'string' },
             email: { type: 'email' },
             password: { type: 'string' },
         })
+
+        if (await this.service.user.findByUsername(body.username)) {
+            ctx.throw(422, '用户已存在!')
+        }
+        if (await this.service.user.findByEmail(body.email)) {
+            ctx.throw(422, '邮箱已存在!')
+        }
         // 2. 保存用户
+
+        const user = await this.service.user.createUser(body)
+
         // 3. 生成token
         // 4. 发送响应
-        ctx.body = 'hi, create12'
+        ctx.body = user
     }
 }
 
---
+++ app/service/user.js
@@ -0,0 +1,29 @@
+const Service = require('egg').Service
+
+class UserService extends Service {
+    get User() {
+        return this.app.model.User
+    }
+
+    findByUsername(username) {
+        return this.User.findOne({
+            username,
+        })
+    }
+
+    findByEmail(email) {
+        return this.User.findOne({
+            email,
+        })
+    }
+
+    async createUser(data) {
+        data.password = this.ctx.helper.md5(data.password)
+        const user = new this.User(data)
+        await user.save() // 保存到数据库
+        return user
+    }
+
+}
+
+module.exports = UserService
